985af44471ace1013b9686ea51acc14d614c81bb
清水翔貴 <shimizushoki@shimizushoutakanoMacBook-Air.local>
2024-10-01 20:34:37 +0900

    データベースにmemoカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_09_25_151733) do
+ActiveRecord::Schema.define(version: 2024_10_01_112315) do
 
   create_table "schedules", force: :cascade do |t|
     t.string "title"
